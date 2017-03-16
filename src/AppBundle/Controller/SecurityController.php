a7aa1edc82ef5bd2e5b1571647a9dbe630d6bfd9
Francois <frdebellescize@hotmail.fr>
2017-03-16 11:58:00 +0000

    user account

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+       $helper = $this->get('security.authentication_utils');
+
+       return $this->render(
+           'security/login.html.twig',
+           array(
+               'last_username' => $helper->getLastUsername(),
+               'error'         => $helper->getLastAuthenticationError(),
+           )
+       );
+    }
+
+    /**
+     * @Route("/login_check", name="security_login_check")
+     */
+    public function loginCheckAction()
+    {
+
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+
+    }
+}
